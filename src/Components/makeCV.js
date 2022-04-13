11e978b989de6d2fe070236cfba8091c30235e98
Nitish Kumar Singh <nitish917085@gmail.com>
2022-04-13 09:08:11 +0530

    resume

---
+++ src/Components/makeCV.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class makeCV extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>makeCV</div>
+    )
+  }
+}
+
+export default makeCV
